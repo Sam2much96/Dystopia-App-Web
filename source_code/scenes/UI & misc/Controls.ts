515b744cc301b1a202da6e7da7cfa13aae4185f7
sam2much96 <sam2much96@gmail.com>
2025-09-22 19:46:36 +0100

    maintenance update 2

---
+++ source_code/scenes/UI & misc/Controls.ts
@@ -0,0 +1,39 @@
+/**
+ * 
+ * Controls 
+ * 
+ * All Game Engine control settings in a single class
+ * 
+ * Features :
+ * (1) Maps the game engine controls to a game hud UI
+ * 
+ * To Do:
+ * (1) Implement kenny UI stylesheet
+ * (2) Map engine settings via Controls class to UI.ts
+ * 
+ */
+
+import * as LittleJS from 'littlejsengine';
+
+
+
+//const {setTouchGamepadAlpha, setTouchGamepadAnalog,setTouchGamepadSize, setTouchGamepadEnable} = LittleJS;
+
+
+
+export class Controls {
+
+
+    constructor(){
+    LittleJS.setShowSplashScreen(false);
+
+    // Game Pad on Mobile Devices Settings
+    LittleJS.setTouchGamepadEnable(true);
+    LittleJS.setTouchGamepadSize(100); // game pad is too big on some mobile browsers
+    LittleJS.setTouchGamepadAlpha(0.3);
+
+    // set dpad configuration on mobile browsers 
+    LittleJS.setTouchGamepadAnalog(false);
+    }
+
+}
