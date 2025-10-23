e3f9bbaba47f6f304827a7b50b6a5c5cc45292b2
sam2much96 <sam2much96@gmail.com>
2025-10-23 18:04:34 +0100

    animation and items optimization 1

--- source_code/scenes/items/Bow.ts
+++ source_code/scenes/items/Bow.ts
@@ -1,9 +1,9 @@
 import * as LittleJS from 'littlejsengine';
 
-const {EngineObject, vec2, drawTile, tile, isOverlapping} = LittleJS;
+//const {EngineObject, vec2, drawTile, tile, isOverlapping} = LittleJS;
+import { Items } from '../../singletons/Utils';
 
-
-export class Bow extends EngineObject{
+export class Bow extends Items{
     /**
      * 
      * Game Bow Object
@@ -13,39 +13,9 @@ export class Bow extends EngineObject{
      * (2) port status queue ui from godot to typescript
      * 
      */
-
-    constructor(posi : LittleJS.Vector2){
-
-        super()
-        this.pos = posi;
-        this.size = vec2(0.7);  
-
-    }
-
-    render(){
-        drawTile(this.pos, this.size, tile(24, 128, 2, 0), this.color, 0, this.mirror);
-    }
-
-    update(){
-
-        if (window.player){
-            // set player collision to coin object
-            // set coin idle animation
-            if (isOverlapping(this.pos, this.size, window.player.pos, window.player.size)) {
-                
-                //console.log("Bow item collected");
-                window.dialogs.show_dialog("Bow collected", "");
-                this.destroy();
-
-                // update bomb count in inventory
-                let y : number = window.inventory.get("Bow");
-                let z : number = y + 1;
-                window.inventory.set("Bow", z);
-                window.music.item_pickup.play();
-                
-            }
-        }
-    }
-
+    //24
+          constructor(pos : LittleJS.Vector2,tileIndex = 24){
+                  super(pos,tileIndex, "Bow");
+              }
 
 }
--- source_code/scenes/items/Coins.ts
+++ source_code/scenes/items/Coins.ts
@@ -1,8 +1,9 @@
 import * as LittleJS from 'littlejsengine';
 
-const {EngineObject, vec2, drawTile, tile, isOverlapping} = LittleJS;
+//const {EngineObject, vec2, drawTile, tile, isOverlapping} = LittleJS;
+import { Items } from '../../singletons/Utils';
 
-export class Coins extends EngineObject {
+export class Coins extends Items {
     /**
      * 
      * Game Coin Object
@@ -11,38 +12,10 @@ export class Coins extends EngineObject {
      * (1) Add coins animation sprites
      * (2) Add ATC Transaction to coin collision 
      */
+    //22
 
-    constructor(posi : LittleJS.Vector2){
-
-        super()
-        this.tileInfo = tile(22, 128, 2, 4); // set coin tile 22
-        this.pos = posi;
-        this.size = vec2(0.7);  
-
-    }
-
-    render(){
-        drawTile(this.pos, this.size, tile(22, 128, 2, 0), this.color, 0, this.mirror);
-    }
-
-    update(){
-
-        if (window.player){
-            // set player collision to coin object
-            // set coin idle animation
-            if (isOverlapping(this.pos, this.size, window.player.pos, window.player.size)) {
-                window.dialogs.show_dialog("sud coins collected", ""); // to do: should ideally be item hud, requires implement stats hud for item collect
-                //console.log("coin collected, creating atc txn");
-                this.destroy();
-
-                window.music.item_pickup.play();
-
-                // to do : 
-                // (1) implement game coin render
-                // (2) implement game coin price statistics calculation
-                
-            }
-        }
-    }
+           constructor(pos : LittleJS.Vector2,tileIndex = 22){
+                   super(pos,tileIndex, "Collectibles");
+               }
 }
 
